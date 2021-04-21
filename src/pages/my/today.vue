5f42c884a4f711a29d1160c2998aa2a51dfc7c44
vimming <vimmingshe@gmail.com>
2021-04-21 17:12:26 +0800

    优化loading

--- src/pages/my/today.vue
+++ src/pages/my/today.vue
@@ -1,45 +1,62 @@
 <template>
   <view class="container">
-    <img src="https://codehub.store/douban/today.png" alt="" mode="widthFix" />
-    <view class="submit--wrap">
-      <button :loading="loading" plain @click="handleSave">下载保存</button>
+    <view v-if="imgSrc">
+      <img :src="imgSrc" alt="" mode="widthFix" />
+      <view class="submit--wrap">
+        <button :loading="loading" plain @click="handleSave">下载保存</button>
+      </view>
     </view>
+    <div class="spinner" v-else>
+      <div class="cube1"></div>
+      <div class="cube2"></div>
+      <div class="loadingText">海报加载中...</div>
+    </div>
   </view>
 </template>
 
 <script>
 export default {
   data() {
-    loading: false;
+    return {
+      loading: false,
+      imgSrc: "",
+    };
+  },
+  onLoad() {
+    this.loading = true;
+    uni.downloadFile({
+      url: "https://codehub.store/douban/today.png",
+      success: (res) => {
+        this.imgSrc = res.tempFilePath;
+        this.loading = false;
+      },
+      fail: () => {
+        uni.showToast({
+          icon: "none",
+          title: "下载失败",
+          duration: 1000,
+        });
+        this.loading = false;
+      },
+    });
   },
   methods: {
     handleSave() {
       this.loading = true;
-      uni.downloadFile({
-        url: "https://codehub.store/douban/today.png",
-        success: (res) => {
-          wx.saveImageToPhotosAlbum({
-            filePath: res.tempFilePath,
-            success: () => {
-              uni.showToast({
-                icon: "none",
-                title: "保存成功",
-                duration: 1000,
-              });
-            },
-            complete: () => {
-              uni.showToast({
-                icon: "none",
-                title: "保存失败",
-                duration: 1000,
-              });
-            },
+      uni.saveImageToPhotosAlbum({
+        filePath: this.imgSrc,
+        success: () => {
+          uni.showToast({
+            icon: "none",
+            title: "保存成功",
+            duration: 1000,
           });
+          this.loading = false;
         },
         fail: () => {
           uni.showToast({
             icon: "none",
-            title: "下载失败",
+            title: "保存失败",
             duration: 1000,
           });
           this.loading = false;
@@ -60,5 +77,69 @@ export default {
 }
 .submit--wrap {
   padding: 60rpx 30rpx;
+}
+.cube1,
+.cube2 {
+  background-color:$uni-color-primary;
+  width: 15px;
+  height: 15px;
+  position: absolute;
+  top: 40%;
+  left: calc(50% - 40px);
+
+  -webkit-animation: sk-cubemove 1.8s infinite ease-in-out;
+  animation: sk-cubemove 1.8s infinite ease-in-out;
+}
+
+.cube2 {
+  -webkit-animation-delay: -0.9s;
+  animation-delay: -0.9s;
+}
+.loadingText{
+  position: absolute;
+  top: 60%;
+  left: 50%;
+  transform: translateX(-50%);
+  color: $uni-color-primary;
+}
+
+@-webkit-keyframes sk-cubemove {
+  25% {
+    -webkit-transform: translateX(42px) rotate(-90deg) scale(0.5);
+  }
+  50% {
+    -webkit-transform: translateX(42px) translateY(42px) rotate(-180deg);
+  }
+  75% {
+    -webkit-transform: translateX(0px) translateY(42px) rotate(-270deg)
+      scale(0.5);
+  }
+  100% {
+    -webkit-transform: rotate(-360deg);
+  }
+}
+
+@keyframes sk-cubemove {
+  25% {
+    transform: translateX(42px) rotate(-90deg) scale(0.5);
+    -webkit-transform: translateX(42px) rotate(-90deg) scale(0.5);
+  }
+  50% {
+    transform: translateX(42px) translateY(42px) rotate(-179deg);
+    -webkit-transform: translateX(42px) translateY(42px) rotate(-179deg);
+  }
+  50.1% {
+    transform: translateX(42px) translateY(42px) rotate(-180deg);
+    -webkit-transform: translateX(42px) translateY(42px) rotate(-180deg);
+  }
+  75% {
+    transform: translateX(0px) translateY(42px) rotate(-270deg) scale(0.5);
+    -webkit-transform: translateX(0px) translateY(42px) rotate(-270deg)
+      scale(0.5);
+  }
+  100% {
+    transform: rotate(-360deg);
+    -webkit-transform: rotate(-360deg);
+  }
 }
 </style>
