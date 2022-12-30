8ba205b96ee9c2862b54e4498ab2cf9568df4acb
VimMing <vimmingshe@gmail.com>
2022-12-30 17:02:12 +0800

    feat: add setting page

---
+++ src/pages/my/setting.vue
@@ -0,0 +1,14 @@
+<template>
+  <view class="form">
+    <form-item label="头像"></form-item>
+    <form-item label="昵称"></form-item>
+  </view>
+</template>
+<script>
+import FormItem from '@/components/form-item/index.vue'
+export default {
+  components: {
+    FormItem,
+  },
+}
+</script>
